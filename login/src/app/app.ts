772a9404f5b447c8af9911ed100a739642804320
Marco <marco.ramiandra@outlook.com>
2026-05-18 02:05:26 +0300

    Add logout button component and authentication check in app

--- login/src/app/app.ts
+++ login/src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthStore } from './auth/store/auth.store';
+import { LogoutButtonComponent } from './auth/components/logout-button/logout-button.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LogoutButtonComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-
+  authStore = inject(AuthStore);
 }
